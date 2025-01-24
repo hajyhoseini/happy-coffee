05cf40caa465911600d0c6dd3a8c4181e012cad8
hajyhoseini <hajy3843@gmail.com>
2025-01-25 01:29:49 +0330

    8

--- components/detailical/happy/contactButton.jsx
+++ components/detailical/happy/contactButton.jsx
@@ -10,43 +10,43 @@ const ContactButtons = () => {
       {/* دکمه تماس با تلفن */}
       <a
         href="tel:+123456789"
-        className={`contact-btn flex items-center justify-center p-4 rounded-full ${
+        className={`contact-btn flex items-center justify-center p-3 rounded-full ${
           isDarkMode ? "bg-[#4E342E] text-green-500" : "bg-[#D7CCC8] text-green-600"
         } hover:bg-[#3E2723] hover:text-green-700 transition-all duration-300`}
       >
-        <FaPhone className="text-4xl" />
+        <FaPhone className="text-2xl" />
       </a>
 
       {/* دکمه ایمیل */}
       <a
         href="mailto:youremail@example.com"
-        className={`contact-btn flex items-center justify-center p-4 rounded-full ${
+        className={`contact-btn flex items-center justify-center p-3 rounded-full ${
           isDarkMode ? "bg-[#3E2723] text-gray-200" : "bg-[#F5F5F5] text-gray-700"
         } hover:bg-[#2C211D] hover:text-gray-800 transition-all duration-300`}
       >
-        <FaEnvelope className="text-4xl" />
+        <FaEnvelope className="text-2xl" />
       </a>
 
       {/* دکمه واتساپ */}
       <a
         href="https://wa.me/yourwhatsappnumber"
         target="_blank"
-        className={`contact-btn flex items-center justify-center p-4 rounded-full ${
+        className={`contact-btn flex items-center justify-center p-3 rounded-full ${
           isDarkMode ? "bg-[#4E342E] text-green-500" : "bg-[#D7CCC8] text-green-600"
         } hover:bg-[#3E2723] hover:text-green-700 transition-all duration-300`}
       >
-        <FaWhatsapp className="text-4xl" />
+        <FaWhatsapp className="text-2xl" />
       </a>
 
       {/* دکمه اینستاگرام */}
       <a
         href="https://wa.me/yourwhatsappnumber"
         target="_blank"
-        className={`contact-btn flex items-center justify-center p-4 rounded-full ${
+        className={`contact-btn flex items-center justify-center p-3 rounded-full ${
           isDarkMode ? "bg-[#4E342E] text-green-500" : "bg-[#D7CCC8] text-green-600"
         } hover:bg-[#3E2723] hover:text-green-700 transition-all duration-300`}
       >
-        <FaInstagram className="text-4xl text-pink-600" />
+        <FaInstagram className="text-2xl text-pink-600" />
       </a>
     </div>
   );
