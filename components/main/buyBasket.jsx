99d254ff2337ae59045ac86cc9884e96c81fea6a
hajyhoseini <hajy3843@gmail.com>
2025-01-27 01:28:52 +0330

    10

--- components/main/buyBasket.jsx
+++ components/main/buyBasket.jsx
@@ -20,7 +20,7 @@ const BuyBasket = () => {
       {/* اگر سبد خرید خالی بود این متن نمایش داده می‌شود */}
       <h2
         className={`text-4xl w-full text-center py-3 px-8 rounded-lg mb-6 shadow-xl transition-all ${
-          isDarkMode ? "text-white bg-yellow-800" : "text-black fontb bg-yellow-600"
+          isDarkMode ? "text-white bg-yellow-800" : "text-black fontb bg-yellow-700"
         }`}
         style={{
           textShadow: "2px 2px 6px rgba(255, 223, 0, 0.6)",
@@ -35,7 +35,7 @@ const BuyBasket = () => {
       {cart.length > 0 ? (
         <Card
           className={`text-center p-6 rounded-xl shadow-xl transition-all transform hover:scale-105 ${
-            isDarkMode ? "bg-yellow-700 text-white" : "bg-yellow-600 text-black"
+            isDarkMode ? "bg-yellow-800 text-white" : "bg-yellow-700 text-black"
           }`}
           style={{
             boxShadow: isDarkMode
@@ -50,27 +50,30 @@ const BuyBasket = () => {
             </h3>
             <ul className="mb-4">
               {cart.map((item, index) => (
-               <li
-               key={index}
-               className={`text-lg p-3 my-2 rounded-md ${
-                 isDarkMode ? "bg-gray-800" : "bg-white"
-               } border-2 border-gray-500`}
-               style={{
-                 boxShadow: isDarkMode
-                   ? "rgba(255, 255, 255, 0.1) 0px 2px 4px"
-                   : "rgba(0, 0, 0, 0.1) 0px 2px 4px",
-               }}
-             >
-               {item.name} - {item.price} تومان - تعداد: {item.quantity}
-               <Button
-                 variant="danger"
-                 size="sm"
-                 className="mt-3 px-8" // افزایش فاصله به ۴
-                 onClick={() => removeFromCart(item.name)} // حذف از سبد خرید
-               >
-                 حذف
-               </Button>
-             </li>
+          <li
+          key={index}
+          className={`text-lg p-3 my-2 rounded-md ${
+            isDarkMode ? "bg-gray-800" : "bg-white"
+          } border-2 border-gray-500 flex justify-between items-center`}
+          style={{
+            boxShadow: isDarkMode
+              ? "rgba(255, 255, 255, 0.1) 0px 2px 4px"
+              : "rgba(0, 0, 0, 0.1) 0px 2px 4px",
+          }}
+        >
+          <span>
+            {item.name} - {item.price} تومان - تعداد: {item.quantity}
+          </span>
+          <Button
+            variant="danger"
+            size="sm"
+            className="ml-4" // دکمه حذف را به راست منتقل می‌کند
+            onClick={() => removeFromCart(item.name)} // حذف از سبد خرید
+          >
+            حذف
+          </Button>
+        </li>
+        
              
               ))}
             </ul>
